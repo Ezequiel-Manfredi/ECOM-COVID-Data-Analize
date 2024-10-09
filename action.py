ea25b34a8ba84068949ed4236e34d6386937d3b7
Ezequiel-Manfredi <ezemanfre@gmail.com>
2024-10-09 16:01:18 -0300

    add: record counter by field value

--- action.py
+++ action.py
@@ -1,6 +1,13 @@
-actions = [
-  {
-    'field': '',
-    'do': lambda x: x
-  }
-]
+class CountBy:
+  def __init__(self,fields):
+    self.selectedFields = {
+      field : {}
+      for field in fields
+    }
+  
+  def process(self,record):
+    for key in self.selectedFields.keys():
+      self.selectedFields[key][record[key]] = self.selectedFields.get(key, 0).get(record[key], 0) + 1
+  
+  def show(self):
+    print(self.selectedFields)
--- main.py
+++ main.py
@@ -13,12 +13,14 @@
 # grupo_etario
 
 from validator import validators
-from action import actions
+from action import CountBy
 
 # row values extracted to a list 
 lineToList = lambda x: x.replace('\n','').split(',')
 
 filePath = './datos/modelo_muestra.csv'
+
+totalizer = CountBy(['sexo','vacuna'])
 
 with open(filePath,'r',encoding='utf-8') as file:
   fields = lineToList(file.readline())
@@ -26,6 +28,10 @@ with open(filePath,'r',encoding='utf-8') as file:
   for line in file:
     values = lineToList(line)
     record = {field: values[index] for index,field in enumerate(fields)}
+    
+    totalizer.process(record)
+  
+  totalizer.show()
   # for field_to_validate in validators:
   #   value = record[field_to_validate['field']]
   #   validations = field_to_validate['validate']
