afbcf202c393d2b3acab98bbdc22e2981809f0a7
Ezequiel-Manfredi <ezemanfre@gmail.com>
2024-10-11 18:59:51 -0300

    add: output message dictionary

---
+++ utils.py
@@ -0,0 +1,13 @@
+FIELDS = {
+  'sexo': {
+    'name': 'Distribución por Género',
+    'M': 'Masculino',
+    'F': 'Femenino'
+  },
+  'jurisdiccion_residencia': {
+    'name': 'Dosis por Jurisdicción de Residencia'
+  },
+  'vacuna': {
+    'name': 'Vacunas Aplicadas por Tipo de Vacuna'
+  }
+}
