9508d6cef77a2151a37b3fde59e1ce71333309a1
Ezequiel-Manfredi <ezemanfre@gmail.com>
2024-10-11 16:11:54 -0300

    add: method comment and total count

--- action.py
+++ action.py
@@ -4,10 +4,23 @@ class CountBy:
       field : {}
       for field in fields
     }
+    self.total = 0
   
   def process(self,record):
+    """
+    - counter of diferent columns values
+    - for each column, for each diferent value
+
+    Args:
+        record (dict): row information
+    """
+    self.total += 1
     for key in self.selectedFields.keys():
       self.selectedFields[key][record[key]] = self.selectedFields.get(key, 0).get(record[key], 0) + 1
   
   def show(self):
+    """
+    - emit counter's report
+    """
+    print(self.total)
     print(self.selectedFields)
