d0ab5fa0c86334fb82503cc40e348c3145258389
Ezequiel-Manfredi <ezemanfre@gmail.com>
2024-10-11 19:01:44 -0300

    remove: comments

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-# resultado_por_genero = [0,0]
-# resultado_por_tipo_vacuna = []
-# total_registros = 0
-
 # segunda_dosis = []
 # mayores_60_refuerzo = 0
-
-# sexo
-# vacuna
-# jurisdiccion_residencia,jurisdiccion_residencia_id
 
 # cod_dosis_generica,nombre_dosis_generica
 # grupo_etario
@@ -32,12 +24,3 @@ with open(filePath,'r',encoding='utf-8') as file:
     totalizer.process(record)
   
   totalizer.show()
-  # for field_to_validate in validators:
-  #   value = record[field_to_validate['field']]
-  #   validations = field_to_validate['validate']
-    
-  #   l = list(map(lambda check: check(value),validations))
-    
-  #   print(l)
-  # print(i)
-  # i += 1
